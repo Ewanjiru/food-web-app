28dbbe00786a4d12cdf99dd74a4ca9bcc38176d8
Eunice Wanjiru <eunice.wanjiru@andela.com>
2018-01-15 13:20:31 +0300

    create the home and vendors component

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Home from '../components/home';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
